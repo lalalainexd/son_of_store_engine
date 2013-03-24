0a7a3520ca6f2056b04f877fa7a6f2ac2457076d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-03-23 18:40:04 -0600

    Added users controller spec and tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,34 @@ require 'spec_helper'
 describe UsersController do
 
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
+    it "renders new view" do
+      get :new
       response.should be_success
+      expect(response).to render_template :new
+    end
+
+    it "assigns new user to @user" do
+      get :new
+      expect(assigns(:user)).to be_kind_of(User)
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "POST 'create'" do
+    let(:valid_attributes) do
+      { user: { email: "user@oregonstore.com", 
+        password: "password" } }
+    end
+
+    it "saves the new user to the database" do
+      expect {
+        post :create, valid_attributes
+      }.to change(User, :count).by(1)
+    end
+
+    it "redirects user to root url" do
+      expect {
+        post :create, valid_attributes
+      }.to redirect_to root_url
     end
   end
 
