1dc1d6305e6b6c46e3bf2c29c69ba577761f1607
Josh Mejia <joshua.mejia@gmail.com>
2013-03-27 17:02:14 -0600

    added add and subtract cart items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,11 +2,11 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Product successfully added to your cart.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -20,8 +20,29 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_path }
+      format.html { redirect_to @line_item.cart }
       format.json { head :no_content }
     end
+  end
+
+  def update
+    # fail
+    @line_item = LineItem.find(params[:id])
+    puts params[:quantity]
+
+    respond_to do |format|
+      if @line_item.update_attributes(params[:line_item])
+        @line_item.update_attribute("quantity", params[:quantity])
+        format.html { redirect_to @line_item.cart, notice: 'Product quanity was updated.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to @line_item.cart, notice: 'Could not update.' }
+        #format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def increase
+    LineItem.increase_quantity
   end
 end
