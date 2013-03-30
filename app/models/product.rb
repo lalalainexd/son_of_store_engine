3553ff3b27e19d567c26f54b34ef5763911b9ba1
John Maddux <jemaddux@gmail.com>
2013-03-30 13:29:43 -0600

    Fixing product retired

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,8 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :image, :category_ids
+  attr_accessible :description, :name, :price, :image, :category_ids, :retired
 
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
-
-  # has_attached_file :image, styles: {
-  #   small: '100x100>',
-  #   medium: '200x200>',
-  #   large: '300x300>',
-  #   x_large: '400x400>',
-  #   xx_large: '500x500>',
-  #   xxx_large: '600x600>'
-  # }
-
 
   has_attached_file :image, styles: {
     small: '100x100>',
