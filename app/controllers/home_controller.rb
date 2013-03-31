a331ae41c099d705b22bd4857033aa50c0d61ae7
John Maddux <jemaddux@gmail.com>
2013-03-31 14:39:41 -0600

    Products are now in partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def show
     @categories = Category.all.sort
     @products = Product.where(retired: false).shuffle
+    @products_array = @products[0..2] 
   end
 end
