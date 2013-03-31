4413308180e9f42f0821cbacdcf13c15fd59b40b
Geoff Schorkopf <gschorkopf@gmail.com>
2013-03-31 09:17:18 -0600

    Made some changes to ability class

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,31 @@
 class OrdersController < ApplicationController
-  # GET /orders
-  # GET /orders.json
   def index
     @orders = Order.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @orders }
     end
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
     @order = Order.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.json { render json: @order }
     end
   end
 
-  # GET /orders/new
-  # GET /orders/new.json
   def new
     @order = Order.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html
       format.json { render json: @order }
     end
   end
 
-  # GET /orders/1/edit
   def edit
     @order = Order.find(params[:id])
   end
@@ -58,8 +51,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PUT /orders/1
-  # PUT /orders/1.json
   def update
     @order = Order.find(params[:id])
 
@@ -74,8 +65,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
     @order = Order.find(params[:id])
     @order.destroy
