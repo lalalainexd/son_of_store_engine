49a9da1194edcded680bb46216ae3093a49a38fc
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-02 20:25:10 -0600

    Flavor to users page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,6 @@
 class Order < ActiveRecord::Base
   attr_accessible :status, :user_id, :total_cost
-
   has_many :line_items, :dependent => :destroy
-
   belongs_to :user
 
   def add_line_items(cart)
