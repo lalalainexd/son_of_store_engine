9030f6aae87f526e4c503de088d40052aab82377
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-03 20:40:10 -0600

    Pending cart tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -18,5 +18,30 @@ describe "Users", js: true do
 
       page.should have_content "Password doesn't match confirmation"
     end
+
+    it "when user renders show without logging in" do
+      visit '/users/1'
+
+      page.should have_content "You are not permitted to view that user"
+    end
+  end
+
+  context "user logs in" do
+    it "can see invoice history" do
+      visit '/signup'
+      fill_in "user_full_name", with: "joe schmo"
+      fill_in 'user_email', with: 'user@oregonsale.com'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button "Sign Up!"
+
+      visit '/login'
+      fill_in 'email', with: 'user@oregonsale.com'
+      fill_in 'password', with: 'password'
+      click_button 'Log in'
+      
+      visit profile_path
+      page.should have_content "Invoices for joe schmo"
+    end
   end
 end
