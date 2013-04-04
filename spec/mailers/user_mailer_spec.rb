6a0bb8a309cebe01651887982c0c9afb57b27693
Josh Mejia <joshua.mejia@gmail.com>
2013-04-03 23:58:17 -0600

    fixed some tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,21 @@ require "spec_helper"
 
 describe UserMailer do
   describe "order_confirmation" do
-    let(:mail) { UserMailer.order_confirmation }
+
+    let(:user) { User.create(full_name: "JoshMejia",
+                             email: "user@oregonsale.com",
+                             password: "password") }
+    let(:order) { Order.create(confirmation: "AAAAAA") }
+    let(:mail) { UserMailer.order_confirmation(user, order) }
 
     it "renders the headers" do
       mail.subject.should eq("Order confirmation")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.to.should eq(["user@oregonsale.com"])
+      mail.from.should eq(["no-reply@oregonsale.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("JoshMejia")
     end
   end
 
