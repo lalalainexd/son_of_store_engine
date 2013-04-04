b52ec26efbdca60e670e66a736396272938ba06f
John Maddux <jemaddux@gmail.com>
2013-04-03 18:00:14 -0600

    Fixed a few tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def user_search
     @products = product_search(params[:search]) 
     @user = current_user
-    @orders = Order.all #order_search(params[:search], current_user)
+    @orders = user_order_search(params[:search], current_user)
     render :user_search
   end
 
@@ -24,14 +24,40 @@ private
     end
   end
 
-  def order_search(search, user)
-    if search && user
-      # orders = Order.where(:user_id => user.id)
-      # order_ids = []
-      # order.each do |order|
-      #   order_ids << order.id
-      # end
-      # LineItem.find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  def user_order_search(search, user)
+    if user
+      orders = Order.where(:user_id => user.id)
     end
+
+    returned_orders = orders
+    
+    if search
+      returned_orders = orders.find(:all, :conditions => ['status LIKE ?', "%#{search}%"])
+    end
+
+    returned_orders
   end
 end
+
+#orders = orders.where(:user_id => user.id)
+# order_ids = []
+# order.each do |order|
+#   order_ids << order.id
+# end
+# LineItem.find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+
+# products = Product.order(:name)
+# products = products.where("name like ?", "%#{keywords}%") if keywords.present?
+# products = products.where(category_id: category_id) if category_id.present?
+# products = products.where("price >= ?", min_price) if min_price.present?
+# products = products.where("price <= ?", max_price) if max_price.present?
+# products
+
+
+
+
+
+
+
+
+
