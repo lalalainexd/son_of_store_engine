803c2bc57a588992660e86476f50dff8b9122bce
lalalainexd <lalalainexd@gmail.com>
2013-04-10 22:53:38 -0600

    modified controller to direct to 404 page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -13,7 +13,7 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     if @store.pending?
-      render status: 404
+      render file: "#{Rails.root}/public/404.html", status: 404
     end
   end
 
