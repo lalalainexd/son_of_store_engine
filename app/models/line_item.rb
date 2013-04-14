4a9b41264bcfa875f984e801eb1bcd6c4eec9892
lalalainexd <lalalainexd@gmail.com>
2013-04-13 19:59:29 -0600

    changed the way line_item's price is being calculated

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :quantity, :order_id, :price
+  attr_reader :total
 
   validates :product_id, presence: true
 
@@ -9,16 +10,18 @@ class LineItem < ActiveRecord::Base
 
   def increase_quantity
     self.quantity += 1
+    self.total = nil
     save
   end
 
   def decrease_quantity
     self.quantity -= 1
+    self.total = nil
     save
   end
 
   def total
-    quantity.to_i * product.price.to_i
+    @total ||= quantity * price
   end
 
   def historical_total
