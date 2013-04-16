69afc4c1293c01592a6e8ea452102573d064d3cc
lalalainexd <lalalainexd@gmail.com>
2013-04-16 15:27:02 -0600

    un-BOOM

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password], params[:remember_me])
     if user && user.platform_administrator
-      raise "BOOM"
         redirect_to '/admin', notice: "Logged in."
     elsif user
         redirect_to previous_page, notice: "Logged in."
