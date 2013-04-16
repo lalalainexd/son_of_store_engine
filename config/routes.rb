55eaaa4dada86b2207ff1877378fea51adccdec3
Austen Ito <austen.ito@gmail.com>
2013-04-15 22:16:04 -0400

    Add new admin to store. :flags:

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,8 @@ StoreEngine::Application.routes.draw do
         end
       end
       resources :orders
+      post "create_admin" => "stores#create_admin"
+      
     end
 
     resources :products, :only => :show
