05156ea7ed21c18bd338c6630b8243dcb513b09d
lalalainexd <lalalainexd@gmail.com>
2013-04-17 01:20:25 -0600

    fixed order model spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,23 +1,24 @@
 require 'spec_helper'
 
 describe Order do
-  include_context "standard test dataset"
-  let!(:new_user){User.create!(full_name: "user",
+  let(:new_user){User.new(full_name: "user",
       email: "user@oregonsale.com", password: "omg42", password_confirmation: "omg42") }
-  let!(:order){Order.create!(status: "pending", user_id: 1, total_cost: 3372)}
-  let!(:li){LineItem.create!(product_id: 1, cart_id: nil,
+  let(:order){Order.new(status: "pending", user_id: 1, total_cost: 3372)}
+  let(:li){LineItem.new(product_id: 1, cart_id: nil,
   order_id: 1, quantity: 3, price: 24)}
 
   describe "generate random" do
     it "generates random thing for different orders" do
+      order.created_at = DateTime.now
       a = order.generate_confirmation_code
 
       new_user = User.new(full_name: "user",
-                          email: "user1@oregonsale.com",
+                          email: "user42@oregonsale.com",
                           password: "omg42",
                           password_confirmation: "omg42")
       order2 = Order.new
-      order.created_at = DateTime.now
+      order2.user = new_user
+      order2.created_at = DateTime.now
 
       expect(order2.generate_confirmation_code).to_not eq a
     end
@@ -31,7 +32,7 @@ describe Order do
       Order.any_instance.should_receive(:save)
       Order.any_instance.should_receive(:add_line_items).with(cart)
       Order.any_instance.should_receive(:save_with_payment).with("4242")
-      Visitor.should_receive(:create).with(email: "foo@bar.com").and_return(visitor)
+      Visitor.should_receive(:find_or_create_by_email).with("foo@bar.com").and_return(visitor)
     end
 
     it "creates a order with a visitor" do
