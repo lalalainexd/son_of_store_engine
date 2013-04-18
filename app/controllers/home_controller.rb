ee910583e6d3acccef21cf8904412fde7312c739
Josh Mejia <joshua.mejia@gmail.com>
2013-04-18 05:16:01 -0600

    tons of seed data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   def show
 #    @categories = Category.all.sort
 #    @products = Category.find_by_name("Essentials").products.shuffle[0..2]
+    count = Product.count
     @products = []
+    3.times { @products << Product.find(1 + Random.rand(count)) }
     @stores = Store.order("name")
   end
 end
